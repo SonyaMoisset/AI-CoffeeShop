c7b544959446c69acc907603d3a392e7c2c1d501
Sonya Moisset <sonya.moisset@gmail.com>
2023-12-04 22:47:43 +0000

    Add search function

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -19,7 +19,16 @@ public class SearchRepository {
     DataSource dataSource;
 
     public List<Product> searchProduct (String input) {
-        return null;
-    }
+        // lowercase the input
+        var lowerInput = input.toLowerCase(Locale.ROOT);
 
+        // create a native query to search for products
+        var query = em.createNativeQuery("Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'", Product.class);
+
+        // get the result list
+        var resultList = (List<Product>) query.getResultList();
+
+        // return the result list
+        return resultList;
+    }
 }
